3e8768be36a409e9572933a6a156e9b414e90b32
Will Cross Clark <will1945@live.co.uk>
2020-08-23 11:38:44 +0100

    #

--- src/Algorithms/MergeSort.js
+++ src/Algorithms/MergeSort.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 
 const mergeSorted = (array) => {
+    visualisationsArray = []
     if (array.length <= 1) {
       return array
     }
--- src/Algorithms/SelectionSort.js
+++ src/Algorithms/SelectionSort.js
@@ -1,25 +1,25 @@
 import React from 'react'
 
 const selectionSorted = (array) => {
-    let visualisationArray = []
+    let visualisationsArray = []
     for (let i = 0; i < array.length; i++) {
         let idxOfMin = i
         for (let j = i + 1; j < array.length; j++) {
             if (array[idxOfMin] > array[j]) {
                 idxOfMin = j
             }
-            visualisationArray.push(["change colour", idxOfMin, j, "first colour"])
-            visualisationArray.push(["change colour", idxOfMin, j, "second colour"])
+            visualisationsArray.push(["change colour", idxOfMin, j, "first colour"])
+            visualisationsArray.push(["change colour", idxOfMin, j, "second colour"])
         } 
         if (idxOfMin != i) {
             let temp = array[i]
             array[i] = array[idxOfMin]
             array[idxOfMin] = temp
-            visualisationArray.push(["change height", i, array[i], idxOfMin, array[idxOfMin]])
+            visualisationsArray.push(["change height", i, array[i], idxOfMin, array[idxOfMin]])
         }
     } 
-    //console.log(visualisationArray)
-    return visualisationArray
+    //console.log(visualisationsArray)
+    return visualisationsArray
 }
 
 export default selectionSorted
